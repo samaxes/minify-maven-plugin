dd503b12ffc2bad5d2ee53f1ec04fe3f1e732c56
samaxes <samaxes@7a4fdc02-5621-11de-992f-fdd7e25b9163>
2009-06-11 02:10:58 +0000

    Demo example

---
+++ demo/src/main/webapp/js/samaxesjs.toc.js
@@ -0,0 +1,68 @@
+/*
+ * The TOC control dynamically builds a table of contents from the headings in
+ * a document and prepends legal-style section numbers to each of the headings.
+ */
+samaxesJS.toc = function() {
+    var window = this, document = this.document;
+
+    /*
+     * Create TOC element links.
+     */
+    function createLink(nodeId, innerHTML) {
+        var a = document.createElement('a');
+        if (nodeId !== '') {
+            a.setAttribute('href', '#' + nodeId);
+        }
+        a.innerHTML = innerHTML;
+        return a;
+    }
+
+    /*
+     * Logging and debugging.
+     */
+    function debug(object) {
+        if (window.console && window.console.log) {
+            window.console.log(object);
+        }
+    }
+
+    return function(toc) {
+        toc = toc.appendChild(document.createElement('ul'));
+        var i2 = 0, i3 = 0, i4 = 0;
+        var nodes = document.body.childNodes;
+
+        for (var i = 0; i < nodes.length; ++i) {
+            var node = nodes[i];
+            var tagName = node.nodeName.toLowerCase();
+            var section;
+            try {
+                if (tagName === 'h4') {
+                    ++i4;
+                    if (i4 === 1) {
+                        toc.lastChild.lastChild.lastChild.appendChild(document.createElement('ul'));
+                    }
+                    section = i2 + '.' + i3 + '.' + i4;
+                    node.insertBefore(document.createTextNode(section + '. '), node.firstChild);
+                    toc.lastChild.lastChild.lastChild.lastChild.appendChild(document.createElement('li')).appendChild(
+                        createLink(node.id, node.innerHTML));
+                } else if (tagName === 'h3') {
+                    ++i3; i4 = 0;
+                    if (i3 === 1) {
+                        toc.lastChild.appendChild(document.createElement('ul'));
+                    }
+                    section = i2 + '.' + i3;
+                    node.insertBefore(document.createTextNode(section + '. '), node.firstChild);
+                    toc.lastChild.lastChild.appendChild(document.createElement('li')).appendChild(
+                        createLink(node.id, node.innerHTML));
+                } else if (tagName === 'h2') {
+                    ++i2; i3 = 0; i4 = 0;
+                    section = i2;
+                    node.insertBefore(document.createTextNode(section + '. '), node.firstChild);
+                    toc.appendChild(document.createElement('li')).appendChild(createLink(node.id, node.innerHTML));
+                }
+            } catch (error) {
+                debug('Error message: ' + error.message);
+            }
+        }
+    };
+}();
