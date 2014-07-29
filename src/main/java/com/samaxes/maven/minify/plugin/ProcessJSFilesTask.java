652f31526a3ace78c5af53e00aa87a3f476634d7
Samuel Santos <samaxes@gmail.com>
2014-07-30 00:08:39 +0100

    Update Google Closure Compiler version

--- src/main/java/com/samaxes/maven/minify/plugin/ProcessJSFilesTask.java
+++ src/main/java/com/samaxes/maven/minify/plugin/ProcessJSFilesTask.java
@@ -30,13 +30,13 @@ import java.io.OutputStreamWriter;
 import java.util.List;
 
 import org.apache.maven.plugin.logging.Log;
+import org.mozilla.javascript.EvaluatorException;
 
 import com.google.common.collect.Lists;
 import com.google.javascript.jscomp.Compiler;
 import com.google.javascript.jscomp.CompilerOptions;
 import com.google.javascript.jscomp.SourceFile;
 import com.google.javascript.jscomp.SourceMap;
-import com.google.javascript.rhino.head.EvaluatorException;
 import com.samaxes.maven.minify.common.ClosureConfig;
 import com.samaxes.maven.minify.common.JavaScriptErrorReporter;
 import com.samaxes.maven.minify.common.YuiConfig;
